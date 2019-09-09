553f3b9b6391f268e4843424899c93c3a48d9b0f
lionel-link <lioneladje@gmail.com>
2019-09-09 21:51:39 +0200

    base front implement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LoginComponent } from './login/login.component';
+import { SingupComponent } from './singup/singup.component';
+import { ItemUserComponent } from './item-user/item-user.component';
+import { ItemMenuComponent } from './item-menu/item-menu.component';
+import { PlayerComponent } from './player/player.component';
+import { SearchComponent } from './search/search.component';
+import { TopComponent } from './top/top.component';
+import { StreamComponent } from './stream/stream.component';
+import { LikeComponent } from './like/like.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+
+const mesRoutes = [
+  { path: '', component: StreamComponent },
+  { path: 'top', component: TopComponent },
+  { path: 'like', component: LikeComponent },
+  { path: 'playlist', component: PlaylistComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    LoginComponent,
+    SingupComponent,
+    ItemUserComponent,
+    ItemMenuComponent,
+    PlayerComponent,
+    SearchComponent,
+    StreamComponent,
+    TopComponent,
+    LikeComponent,
+    PlaylistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(mesRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
