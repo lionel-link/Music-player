3c7fd7144611b40edd9688cec498e402ca9d4fb5
lionel-link <lioneladje@gmail.com>
2019-09-12 16:43:36 +0200

    login implemented

--- src/app/item-user/item-user.component.ts
+++ src/app/item-user/item-user.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ComponentFactoryResolver } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-user',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private resolver:ComponentFactoryResolver, router: Router) { }
 
   ngOnInit() {
   }
